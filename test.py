e4f3db0f6c8adb2482aadcab63ff801f9b540718
srinivas365 <m.srinivas365@gmail.com>
2018-10-08 01:15:56 +0530

    Finalized

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from pca_gene import *
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 dataset=pd.read_csv('gene_data.csv')
 metaset=pd.read_csv('meta.csv')
@@ -9,12 +11,12 @@ metaset=pd.read_csv('meta.csv')
 dataset=dataset.transpose()
 dataset.drop(['Unnamed: 0'],inplace=True)
 dataset.drop(['symbol'],inplace=True)
-dataset=dataset.replace({'ssssss': '39.0', 'hhhh' : '321.43'}, regex=True)
+
 
 
 
 #importing GenePca class
-genepca=GenePca(dataset,metaset['Time'])
+genepca=GenePca(dataset,metaset['Cluster'])
 
 #filling nan values with mean
 genepca.fill_nans(measure='mean')
@@ -23,9 +25,61 @@ genepca.fill_nans(measure='mean')
 genepca.set_components(2)
 
 #reduced dataset
-red_set=genepca.get_reduced_set(std_scale=1)
-print(red_set)
+dataset=genepca.get_reduced_set(std_scale=1)
+pca=genepca.getpca()
+
 print('Explained variance:',genepca.get_ev())
 print('Explained variance ratio:',genepca.get_evr())
+
+
+
+x_train,x_test,y_train,y_test=train_test_split(dataset,metaset['Cluster'],test_size=0.3,random_state=20)
+
+
+'''
+Random forest classifier has good accuracy when compared to 
+other classification
+algorithms
+
+'''
+
+#random forest 
+from sklearn.ensemble import RandomForestClassifier
+classifier=RandomForestClassifier(n_estimators=10,criterion='entropy',random_state=0)
+
+
+
+'''
+#LogisticRegression
+from sklearn.linear_model import LogisticRegression
+classifier=LogisticRegression(random_state=0)
+
+#naive bayes
+from sklearn.naive_bayes import GaussianNB
+classifier=GaussianNB()
+
+
+#random forest 
+from sklearn.ensemble import RandomForestClassifier
+classifier=RandomForestClassifier(n_estimators=10,criterion='entropy',random_state=0)
+
+
+#kmeans 
+from sklearn.neighbors import KNeighborsClassifier
+classifier=KNeighborsClassifier(n_neighbors=5,metric='minkowski',p=2)
+
+'''
+
+#training dataset
+classifier.fit(x_train,y_train)
+
+#testing dataset
+y_pred=classifier.predict(x_test)
+print('Actaul--Predicted')
+for i,j in zip(y_test.values,y_pred):
+	print(i,'-',j)
+
+
+
 #visualizing
 genepca.visualize()
